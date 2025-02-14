f3f46646a0c29681e32077b58c7e4c9d2c667848
plr <paul.thomas.stras@gmail.com>
2025-02-14 22:03:51 +0100

    Premier commit : mise en place de la structure du projet, de la première page statique ainsi que des premiers éléments (header et footer)

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const scrollProgress = document.getElementById("scrollProgress")
+const headerHamburger = document.getElementById("headerHamburger")
+const headerLinks = document.getElementById("headerLinks")
+const headerIcon = document.getElementById("headerIcon")
+
+window.onscroll = function () {
+    updateProgressBar();
+};
+
+function updateProgressBar() {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;  // Position du scroll par rapport au haut de la page
+    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;  // Hauteur de la page
+    scrollProgress.value = (scrollTop / scrollHeight) * 100;  // Pourcentage de la page scrollée
+    scrollProgress.classList.toggle("visible", scrollTop > 10);  // Faire apparaître la barre de progression après un léger scroll
+}
+
+headerHamburger.addEventListener("click", function () {
+    headerIcon.innerText = headerLinks.classList.toggle("visible") ? "close" : "menu";
+});
