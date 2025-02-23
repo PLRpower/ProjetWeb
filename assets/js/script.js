69e625d9311e30e827d90585fe93195c5eca140e
plr <paul.thomas.stras@gmail.com>
2025-02-23 12:28:18 +0100

    Modifications générales

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const scrollProgress = document.getElementById("scrollProgress")
 const headerHamburger = document.getElementById("headerHamburger")
 const headerLinks = document.getElementById("headerLinks")
 const headerIcon = document.getElementById("headerIcon")
+const headerLogin = document.getElementById("headerLogin")
 
 window.onscroll = function () {
     updateProgressBar();
@@ -12,6 +13,7 @@ function updateProgressBar() {
     const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;  // Hauteur de la page
     scrollProgress.value = (scrollTop / scrollHeight) * 100;  // Pourcentage de la page scrollée
     scrollProgress.classList.toggle("visible", scrollTop > 10);  // Faire apparaître la barre de progression après un léger scroll
+    headerLogin.classList.toggle("compact", scrollTop > 10);
 }
 
 headerHamburger.addEventListener("click", function () {
