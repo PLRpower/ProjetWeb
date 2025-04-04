ed4b34e841acf9fbaef2e6267c82d3320dc5c9aa
PLR <paul.thomas.stras@gmail.com>
2025-04-04 03:10:02 +0200

    Ajout de la branche static

--- database/migrations/02_create_admins_table.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Database\Capsule\Manager as Capsule;
-
-Capsule::schema()->create('admins', function ($table) {
-    $table->foreignId('id')->primary()->constrained('users')->onDelete('cascade');
-});
